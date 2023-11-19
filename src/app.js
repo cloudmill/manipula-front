ef16bdc52bf23efd12cfc55892540da563b7a673
kirill matveev <dejar@protonmail.com>
2023-11-19 13:49:36 +0300

    updates

--- src/app.js
+++ src/app.js
@@ -5,44 +5,44 @@ import './styles/app.scss'
 // import './scripts/forms'
 
 // import { swiperInit } from './scripts/sliders.ts'
-// import { fancyboxInit } from './scripts/fancybox.ts'
+import { fancyboxInit } from './scripts/fancybox.ts'
 // import { tooltip } from './scripts/tippy'
 // import { dropdown } from './scripts/dropdown'
 // import { tabs } from './scripts/tabs'
 // import { chickAnimation } from './scripts/main-animations'
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   const loader = document.getElementById('loader-main')
-//   if (loader) {
-//     document.querySelector('body').style.overflow = 'hidden'
-//   }
+window.addEventListener('DOMContentLoaded', () => {
+  //   const loader = document.getElementById('loader-main')
+  //   if (loader) {
+  //     document.querySelector('body').style.overflow = 'hidden'
+  //   }
 
-//   fancyboxInit()
-//   tooltip()
-//   dropdown()
-//   tabs()
+  fancyboxInit()
+  //   tooltip()
+  //   dropdown()
+  //   tabs()
 
-//   // на главной свайпер за пускается по load
-//   if (!document.querySelector('[data-slider-id=main]')) {
-//     swiperInit()
-//   }
+  //   // на главной свайпер за пускается по load
+  //   if (!document.querySelector('[data-slider-id=main]')) {
+  //     swiperInit()
+  //   }
 
-//   const searchReset = document.querySelector('[data-search-reset]')
+  //   const searchReset = document.querySelector('[data-search-reset]')
 
-//   if (searchReset) {
-//     // console.log(searchReset)
-//     searchReset.addEventListener('click', () => {
-//       console.log(document.querySelector('[data-search]'))
-//       document.querySelector('[data-search]').value = ''
-//     })
-//   }
+  //   if (searchReset) {
+  //     // console.log(searchReset)
+  //     searchReset.addEventListener('click', () => {
+  //       console.log(document.querySelector('[data-search]'))
+  //       document.querySelector('[data-search]').value = ''
+  //     })
+  //   }
 
-//   const insertScript = document.querySelector('[data-insert-script]')
-//   if (insertScript) {
-//     insertScript.innerText =
-//       "function cityChoose(name) {document.querySelector('[data-cities-drop-inner]').innerText = name}"
-//   }
-// })
+  //   const insertScript = document.querySelector('[data-insert-script]')
+  //   if (insertScript) {
+  //     insertScript.innerText =
+  //       "function cityChoose(name) {document.querySelector('[data-cities-drop-inner]').innerText = name}"
+  //   }
+})
 
 // window.addEventListener('load', () => {
 //   const loader = document.getElementById('loader-main')
