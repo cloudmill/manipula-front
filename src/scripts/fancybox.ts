89c80fbec95f4e4ae8ddc002af5b66d4aa1525a6
kirill matveev <dejar@protonmail.com>
2023-11-29 00:50:19 +0300

    add catalog-modal

--- src/scripts/fancybox.ts
+++ src/scripts/fancybox.ts
@@ -2,7 +2,7 @@ import { Fancybox } from '@fancyapps/ui'
 import '@fancyapps/ui/dist/fancybox/fancybox.css'
 
 export function fancyboxInit() {
-  Fancybox.defaults = {
+  const defaults = {
     ...Fancybox.defaults,
     dragToClose: false,
   }
@@ -13,6 +13,19 @@ export function fancyboxInit() {
     button?.addEventListener('click', () => {
       // console.log('CLIIICK', id)
       Fancybox.close()
+
+      if (id === 'catalog') {
+        Fancybox.defaults = {
+          ...Fancybox.defaults,
+          tpl: {
+            closeButton:
+              '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" tabindex="-1"><path d="M20 20L4 4m16 0L4 20"/></svg></button>',
+            main: `<div class="fancybox__container down" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1"><div class="fancybox__backdrop"></div><div class="fancybox__carousel"></div><div class="fancybox__footer"></div></div>`,
+          },
+        }
+      } else {
+        Fancybox.defaults = defaults
+      }
 
       // @ts-ignore
       Fancybox.show([{ src: `#fancy-modal-${id}`, type: 'inline' }])
