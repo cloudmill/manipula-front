36ecf4930687ba6c26b95441336bc1f4d7586637
kirill matveev <dejar@protonmail.com>
2023-11-29 11:09:23 +0300

    upd catalog modal

--- src/scripts/fancybox.ts
+++ src/scripts/fancybox.ts
@@ -22,13 +22,29 @@ export function fancyboxInit() {
               '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" tabindex="-1"><path d="M20 20L4 4m16 0L4 20"/></svg></button>',
             main: `<div class="fancybox__container down" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1"><div class="fancybox__backdrop"></div><div class="fancybox__carousel"></div><div class="fancybox__footer"></div></div>`,
           },
+          on: {
+            reveal: (fancybox, slide) => {
+              console.log('CAT MOD OPEN', button)
+
+              button.classList.add('active')
+            },
+            close: (fancybox, slide) => {
+              console.log('CAT MOD CLOSE', button)
+
+              button.classList.remove('active')
+            },
+          },
         }
       } else {
         Fancybox.defaults = defaults
       }
 
-      // @ts-ignore
-      Fancybox.show([{ src: `#fancy-modal-${id}`, type: 'inline' }])
+      if (!button.classList.contains('active')) {
+        console.log(button.classList)
+
+        // @ts-ignore
+        Fancybox.show([{ src: `#fancy-modal-${id}`, type: 'inline' }])
+      }
     })
   })
 }
